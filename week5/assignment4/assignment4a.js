ab427490c59e364cfb29bbcb033f76d0e8290779
Brandon Carpenter <bhcarpenter@gmail.com>
2013-02-07 19:51:16 -0500

    Assignment 4 - winter 2013

--- week5/assignment4/assignment4a.js
+++ week5/assignment4/assignment4a.js
@@ -1,46 +1,48 @@
 /* Part 1: Tip Calculator
- * For this assignment you're going to build a website "frontend" for the tip
- * calculator that you wrote in Assignment #3. If your tip calculator/splitter
- * from Assignment #3 doesn't work, you may use the posted solution instead.
  *
- * Here's what I want you to do:
- * 1. Add an event listener to the "submit" event of the tipCalculator form on
- *    the page.
- * 2. In your event listener:
- *    - Call the preventDefault() method of the event object so that the form
- *      doesn't reload the web page.
- *    - Get the value of the billAmount text field
- *    - Get the value of the percentageToTip text field
- *    - Get the value of the numberOfPeople text field
- *    - Convert all the values into number, and run them through your tip
- *      calculator/splitter
- *    - Display the result in the amountEachPersonShouldPay <div>
- * 3. Make the page look better. Feel free to edit the CSS and HTML.
- *
- * You may not use jQuery for this part. Here are the DOM functions that I
- * think you may need:
- *
- * document.addEventListener()
- * document.getElementById()
- * document.createTextNode()
- * someElement.appendChild()
- * someEvent.preventDefault()
- *
- * Note: The current value entered by a user into a text field is available as
- * the "value" property of the corresponding DOM element.
+ * For this assignment, you're going to build a "tip calculator".
  */
 
-// TODO: Paste your tip calculator functions from Assignment #3 here.
-
-document.addEventListener('DOMContentLoaded', function() {
-
-    // Put all of your code that attaches event handlers to DOM elements inside
-    // of this function. It will be executed when the DOM is ready
-    // to be messed with.
-    // Note: This doesn't work in old versions of IE, so test your homework in
-    // Firefox, Safari, or Chrome.
+/* Step 1: Write a tip calculator!
+ * Write a function called "calculateTip". The "calculateTip" function should
+ * accept two parameters: the amount of the bill to calculate a tip for, and
+ * the percentage to tip. It should **return** the amount of tip you should 
+ * pay.
+ */
 
 
-    // TODO: Your code goes here.
 
-});
+/* Step 2: Test your tip calculator
+ * Invoke your tip calculator a few times with different arguments (just make
+ * them up at this point) and log the results to the console to make sure your
+ * function is working correctly.
+ */
+
+
+/* Step 3: Hook your tip calculator function up to the web page!
+ *
+ * Here's what I want you to do:
+ * 1. Add an event listener to the "submit" event of the tipCalculator <form>
+ *    on the page.
+ * 2. In your event listener:
+ *    - Call the preventDefault() method of the event object. If you don't,
+ *      then submitting the form will take you to the Creative Circus
+ *      website.
+ *    - alert "It's working!" using the alert() method.
+ *
+ * 3. Once you're event listener is working, delete the alert and do these
+ *    steps inside of your event listener instead:
+ *    - Get the value of the billAmount text field
+ *    - Get the value of the percentageToTip text field
+ *    - Convert both values into numbers, and run them through your tip
+ *      calculator.
+ *    - Add the tip to the original amount and display the result 
+ *      in the totalAmountToPay <div>
+ *
+ * Note: The currently entered text in any text field is stored as the "value"
+ * attribute on it's DOM element. This is a **string**, so you'll need to use
+ * one of the techniques that you know to turn a string into a number.
+ */
+
+/* Step 4: Make the page look better by editing the CSS and HTML. */
+
